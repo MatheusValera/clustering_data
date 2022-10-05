784c3fc263dabd6a79cef3a22905907cabe477d8
MatheusValera <lrfp1000@.gmail.com>
2022-10-05 17:29:05 -0300

    feat: import libs and read database

---
+++ src/script.py
@@ -0,0 +1,13 @@
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+# %%
+df = pd.read_csv('../BancoDeDados.csv')
+# %%
+df.info()
+# %%
+df.head()
+# %%
