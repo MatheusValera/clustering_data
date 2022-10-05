4a7f7686d36defc4d98f71d8ed17fcd3d182c1ee
MatheusValera <lrfp1000@.gmail.com>
2022-10-05 18:07:35 -0300

    feat: create df with client recency

--- src/script.py
+++ src/script.py
@@ -11,3 +11,39 @@ df.info()
 # %%
 df.head()
 # %%
+def plot_perc(st,dados):
+  plt.figure(figsize = (20,8))
+  
+  g = sns.countplot(x = st, data = dados, orient = 'h')
+  g.set_ylabel('Contagem', fontsize = 17)
+  
+  sizes = []
+  
+  for p in g.patches:
+    height = p.get_height()
+    sizes.append(height)
+    g.text(p.get_x() + p.get_width() / 1.6,
+           height + 200,
+           '{:1.2f}%'.format(height / 116581 * 100),
+           ha = 'center',
+           va = 'bottom',
+           fontsize = 12)
+    g.set_ylim(0, max(sizes) * 1.1)
+    
+# %%
+plot_perc('estado_cliente', df)
+# %%
+plot_perc('estado_vendedor', df)
+# %%
+plot_perc('pagamento_tipo', df)
+# %%
+df_olist = df[['id_unico_cliente','id_cliente','horario_pedido','item_id','preco']]
+df_compra = df.groupby('id_unico_cliente').horario_pedido.max().reset_index()
+df_compra.columns = ['id_unico_cliente', 'DataMaxCompra']
+df_compra['DataMaxCompra'] = pd.to_datetime(df_compra['DataMaxCompra'])
+# %%
+# Recencia
+df_compra['Recencia'] = (df_compra['DataMaxCompra'].max() - df_compra['DataMaxCompra']).dt.days
+df_usuario = pd.merge(df_olist,df_compra[['id_unico_cliente','Recencia']], on = 'id_unico_cliente')
+df_usuario
+# %%
